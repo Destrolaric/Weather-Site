4a011e21cac7c8e8afce663ac2d10bf3ba2e963a
Destolaric <georgi_spb_2000@mail.ru>
2021-04-02 19:57:14 +0300

    small changes

--- js/printNewCity.js
+++ js/printNewCity.js
@@ -12,7 +12,7 @@ function printNewCity(response, id) {
 
 function fillUL(ul, response) {
     let params = ul.querySelectorAll('.weatherInfo');
-    params[1].textContent = response.data[0].wind_spd;
+    params[1].textContent = response.data[0].wind_spd + "m/s";
     params[3].textContent = response.data[0].weather['description'];
     params[5].textContent = response.data[0].pres;
     params[7].textContent = response.data[0].rh + '%';
